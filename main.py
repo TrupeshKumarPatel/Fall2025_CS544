a13cce29384147a7f3c3b5f315e58023da5922e9
Trupeshkumar Patel <trpatel2@ua.edu>
2025-11-08 13:39:04 -0600

    fixing code based on security code

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import subprocess
 from flask import request, Flask
+
 app = Flask(__name__)
+
 @app.route("/ping")
 def ping():
-target = request.args.get("host", "127.0.0.1")
-# ❌ Vulnerable: untrusted input passed to shell
-subprocess.check_output(f"ping -c 1 {target}", shell=True)
-return "OK"
-# FIX: avoid shell, pass a list; validate/whitelist target
-# subprocess.check_output(["ping", "-c", "1", target])
+    target = request.args.get("host", "127.0.0.1")
+    # ❌ Vulnerable: untrusted input passed to shell
+    # subprocess.check_output(f"ping -c 1 {target}", shell=True)
+
+    # FIX: avoid shell, pass a list; validate/whitelist target
+    subprocess.check_output(["ping", "-c", "1", target])
+
+    return "OK"
